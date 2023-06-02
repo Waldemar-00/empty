bee2e81ef46b8eae7a00618bc54b8454bb812076
uladzimir <vova108vova@gmail.com>
2023-06-02 21:54:14 +0300

    transfer DOM-refs to the parent element

--- src/InsertText.js
+++ src/InsertText.js
@@ -4,28 +4,40 @@ import './App.css'
 class InsertText extends React.Component {
   constructor(props) {
     super(props)
-    // this.inputRef = React.createRef()
     this.elementRef = null
     this.putInElementRef = (element) => {
       this.elementRef = element
     }
   }
   componentDidMount() {
-    // if (this.elementRef) this.onClickButton()
-    this.elementRef.focus() //API of browser, it is using after create DOM
+    console.log(this.elementRef)
+    this.elementRef.inputRef.focus()
+     //API of browser, it is using after create DOM
+    // no API for class
   }
-  // onClickButton = () => {
-    //this.inputRef.current.focus()
-    // if(this.elementRef) this.elementRef.focus()
-  // }
   render() {
     return (
       <>
-        <input className='inputRef' type="text" name="name" ref={this.putInElementRef}></input>
-        <button className='btnRef'onClick={this.onClickButton}>Click me for focus!</button>
+        <CreateInput ref={this.putInElementRef} />
+        {/* no API for class */}
+        <button className='btnRef'>Click me for focus!</button>
       </>
     )
   }
-
 }
+class CreateInput extends React.Component {
+  constructor(props) {
+    super(props)
+    this.inputRef = null
+  }
+  putInRef = (element) => {
+    this.inputRef = element
+  }
+  render() {
+    return (
+      <input className='inputRef' type="text" name="name" ref={this.putInRef}></input>
+      )
+    }
+  }
+
 export default InsertText
