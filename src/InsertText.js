142d54d74571d9ba0fd352ccebb63fb37f01e65b
uladzimir <vova108vova@gmail.com>
2023-06-02 20:32:22 +0300

    used callback-ref

--- src/InsertText.js
+++ src/InsertText.js
@@ -4,19 +4,24 @@ import './App.css'
 class InsertText extends React.Component {
   constructor(props) {
     super(props)
-    this.inputRef = React.createRef()
+    // this.inputRef = React.createRef()
+    this.elementRef = null
+    this.putInElementRef = (element) => {
+      this.elementRef = element
+    }
   }
-  componentDidMount() {
-    this.onClickButton()
-  }
+  // componentDidMount() {
+    // if (this.elementRef)this.onClickButton()
+  // }
   onClickButton = () => {
-    this.inputRef.current.focus()
+    // this.inputRef.current.focus()
+    if(this.elementRef) this.elementRef.focus()
   }
   render() {
     return (
       <>
-        <input className='inputRef' type="text" name="name" ref={this.inputRef}></input>
-        <button onClick={this.onClickButton}>Click me for focus!</button>
+        <input className='inputRef' type="text" name="name" ref={this.putInElementRef}></input>
+        <button className='btnRef'onClick={this.onClickButton}>Click me for focus!</button>
       </>
     )
   }
