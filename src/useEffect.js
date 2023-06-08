bca7bd2be8ebf40e93955e7707a25ada603c7b81
uladzimir <vova108vova@gmail.com>
2023-06-08 21:23:35 +0300

    useEffect with dependence from another useState

--- src/useEffect.js
+++ src/useEffect.js
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react"
 const Counter = () => {
   const [count, setCount] = useState(0)
+  const [multi, setMulti] = useState(0)
   
   useEffect(() => {
-    setTimeout(() => {
-      setCount(count => count + 1)
-    }, 1000)
-  }, [])
+    // setTimeout(() => {setCount(count => count + 1)}, 1000)
+    setMulti(() => count * 2)
+  }, [count])
   const onChangeCounter = () => {
     setCount(count => count + 1)
   }
   const styles = {
-    h1: {
+    h2: {
       textAlign: "center",
       margin: '50px auto',
     },
@@ -22,8 +22,9 @@ const Counter = () => {
   }
   return (
     <>
-    <h2 style={styles.h1}>Value of the Counter: {count} </h2>
+    <h2 style={styles.h2}>Value of the Counter: {count} </h2>
       <button onClick={onChangeCounter} style={styles.btn}>Call counter</button>
+    <h2 style={styles.h2}>After multiplication: {multi} </h2>
     </>
   )
 
